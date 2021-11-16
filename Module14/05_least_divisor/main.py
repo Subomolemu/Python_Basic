d6d6da4b0fa19758e61623f5a4d9a297f00a3133
Sergey Sinkov <subomolemu@gmail.com>
2021-11-16 19:03:08 +0300

    Решение задачи 5. Наименьший делитель.

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+def min_div(num):
+    while num < 1:
+        print('Ошибка ввода.')
+        num = int(input('Введите число n > 0: '))
+
+    for divider in range(2, num + 1):
+        if num % divider == 0:
+            print('Наименьший делитель, отличный от едиицы:', divider)
+            break
+
+
+number = int(input('Введите число n > 0: '))
+
+min_div(number)
