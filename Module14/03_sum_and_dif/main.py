6c386a9d4dbba20e89aec770a2927710498953e3
Sergey Sinkov <subomolemu@gmail.com>
2021-11-16 17:58:04 +0300

    Решение задачи 3. Сумма и разность.

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def sum_number(num, nums):
+    while num > 0:
+        nums += num % 10
+        num //= 10
+    print('Сумма цифр:', nums)
+    return nums
+
+
+def count_number(num, count_num):
+    while num > 0:
+        num //= 10
+        count_num += 1
+    print('Колличество цифр:', count_num)
+    return count_num
+
+
+number = int(input('Введите число:'))
+count = 0
+sumNum = 0
+
+print()
+print('Разность суммы и колличества цифр:', sum_number(number, sumNum) - count_number(number, count))
