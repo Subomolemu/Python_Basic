686e231119f6664fb3f292c4c468f6578c7f50be
Sergey Sinkov <subomolemu@gmail.com>
2021-11-16 19:15:44 +0300

    Решение задачи 6. Монетка

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def coin_search():
+    if x * 2 <= r and y * 2 <= r:
+        print('Монетка где-то рядом.')
+    else:
+        print('Монетки в области нет.')
+
+
+print('Введите координаты понетки: ')
+x = float(input('X = '))
+y = float(input('Y = '))
+r = int(input('Введите радиус: '))
+coin_search()
