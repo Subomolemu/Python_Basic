198721838c3c2082fbb3797ff1bdaf12b01fa3cd
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 17:58:21 +0300

    Решение задачи 1. Список нечетных чисел.

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+number = int(input('Введите число: '))
+odd_nums = []
+
+for num in range(1, number + 1):
+    if num % 2 != 0:
+        odd_nums.append(num)
+
+print(odd_nums)
