1bb1d25c69ce879bb0f9c0c68fd4d84636d4d583
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 18:13:05 +0300

    Решеие задачи 2. Список спортсменов с положительнными индексами.

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+sportsman_list = ['Артемий', 'Борис', 'Влад', 'Гоша', 'Дима', 'Евгений', 'Женя', 'Захар']
+count = 0
+first_day_list = []
+
+for name in sportsman_list:
+    if count % 2 == 0:
+        first_day_list.append(name)
+    count += 1
+
+print('Список участников турнира на первый день:', )
+print(first_day_list)
