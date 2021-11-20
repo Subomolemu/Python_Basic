10260d2f1d268afc747b8ed81204a78b6594ce5a
Sergey Sinkov <subomolemu@gmail.com>
2021-11-21 00:23:14 +0300

    Решение задачи 10. Сортировка по возрастанию

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def sort_numbers(list_nums):
+    for min_i in range(len(list_nums)):
+        for current in range(min_i, len(list_nums)):
+            if list_nums[current] < list_nums[min_i]:
+                list_nums[current], list_nums[min_i] = list_nums[min_i], list_nums[current]
+
+
+n = int(input('Введите колличество элементов в списке: '))
+numbers = []
+
+for _ in range(n):
+    num = int(input('Введите число: '))
+    numbers.append(num)
+
+print('=' * 20, '\nИзначальный список:', numbers)
+sort_numbers(numbers)
+print('\nОтсортированный список', numbers)
