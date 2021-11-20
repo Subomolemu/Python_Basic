5f85e55726bfe9199389dafd50b2fa84cfd69a7b
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 23:16:04 +0300

    Решение задачи 9. Определение палиндрома

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+symbols = []
+flag = False
+
+revers_symbols = []
+
+for symbol in word:
+    symbols.append(symbol)
+
+
+for i in range(-1, -len(symbols) - 1, -1):
+    revers_symbols.append(symbols[i])
+    if symbols[-len(symbols) - i - 1] != revers_symbols[-i - 1]:
+        print('\nСлово не является палиндромом.')
+        break
+    else:
+        flag = True
+
+if flag:
+    print('\nСлово является палиндромом.')
