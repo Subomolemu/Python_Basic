fe4cc486cc641ce06623a367cc291a8b1f86fd19
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 18:25:00 +0300

    Решение задачи 3. Вывод неподходящих значений эффективности

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+all_cells = int(input('Введите колличество клеток: '))
+inefficiency = ''
+
+for i in range(all_cells):
+    print('Эффективнность', i + 1, 'клетки: ', end='')
+    efficiency = int(input())
+    if efficiency < i:
+        inefficiency += str(efficiency) + ' '
+
+print('\nНеподходящие значения:', inefficiency)
