20017de3e619e26f4cdab00629242b5fb7b50f70
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 19:10:10 +0300

    Решение задачи 5. Формирование списка любимых фильмов.

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,28 @@
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+like_films = []
+in_stock = False
 
-# TODO здесь писать код
+print('Для окончания формирования списка введите: end')
+movie_title = input('Введите название любимого фильма: ')
+
+while movie_title != 'end':
+
+    for i in range(9):
+        if films[i] == movie_title:
+            like_films.append(films[i])
+            print('В список добавлен фильм: "' + films[i] + '".\n')
+            in_stock = False
+            break
+        else:
+            in_stock = True
+
+    if in_stock:
+        print('\nОшибка. Такого фильма нет на сайте.\n')
+    in_stock = False
+
+    movie_title = input('Введите название любимого фильма: ')
+
+print('=' * 25, '\nКонец формирования списка.')
+print('Список любимых фильмов:', like_films)
