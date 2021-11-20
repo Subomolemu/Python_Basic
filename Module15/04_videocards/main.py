77727aa4d430a6b1cf3993f552b714cc0fe9d18e
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 18:36:34 +0300

    Решение 4 задачи. Список видео карт без старшего поколения(разобрали)

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,20 @@
-# TODO здесь писать код
+total_graphics_cards = int(input('Введите колличество видеокарт: '))
+cards_list = []
+new_cards = 0
+new_cards_list = []
+
+for num in range(total_graphics_cards):
+    print(num + 1, 'видеокарта: ', end='')
+    graphics_card = int(input())
+    cards_list.append(graphics_card)
+    if new_cards < graphics_card:
+        new_cards = graphics_card
 
 
+print('\nСтарый список видеокарт:', cards_list)
+
+for cards in cards_list:
+    if cards != new_cards:
+        new_cards_list.append(cards)
+
+print('Новый список видеокарт:', new_cards_list)
