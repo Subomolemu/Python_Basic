c6f15643cd5c3cf50afcb8e2b0c821937cf1878f
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 22:00:37 +0300

    Решение задачи 6. Поиск колличества уникальных букв в слове.

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+symbols = []
+count_sym = 0
+count = 1
+
+for symbol in word:
+    symbols.append(symbol)
+
+for i in range(len(symbols)):
+
+    for sym in symbols:
+        if symbols[i] != sym:
+            count += 1
+
+    if count == len(symbols):
+        count_sym += 1
+    count = 1
+
+print('Колличество уникальных букв:', count_sym)
