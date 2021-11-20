8636c36ecc35e598c04f3339a1a6cbbaec0df37c
Sergey Sinkov <subomolemu@gmail.com>
2021-11-20 22:47:24 +0300

    Решение задачи 8. Бегущая строка

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+num = int(input('Введите колличество элементов в списке: '))
+shift = int(input('Введите значение сдвига: '))
+numbers = []
+shift_numbers = []
+
+for _ in range(num):
+    number = int(input('Введите элемент списка: '))
+    numbers.append(number)
+
+print('Изначальный список: ', numbers)
+
+for i in range(-shift, num - shift):
+    shift_numbers.append(numbers[i])
+
+print('Сдвинутый список:', shift_numbers)
