a9e6d6f8be3dbd4522887f470124b878389e7f25
Sergey Sinkov <subomolemu@gmail.com>
2021-11-23 12:28:57 +0300

    Модуль 16. Решение задачи 4

--- Module16/04_party/main.py
+++ Module16/04_party/main.py
@@ -1,3 +1,36 @@
-guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+def came(name):
+    if len(guests) >= 6:
+        print('Прости,', name + ', мест больше нет.')
+    else:
+        print('Привет,', name + '!')
+        guests.append(name)
+    print('\nСейчас на вечеринке', len(guests), 'человек:', guests)
 
-# TODO здесь писать код
+
+def gone(name):
+    print('Пока,', name + '!')
+    guests.remove(name)
+    print('\nСейчас на вечеринке', len(guests), 'человек:', guests)
+
+
+guests = ['Петя', 'Ваня', 'Саша', 'Лиза', 'Катя']
+end = 'пора спать'
+
+print('Сейчас на вечеринке', len(guests), 'человек:', guests)
+print('Гость пришел или ушел? - ', end='')
+ask = input()
+
+
+while ask != end:
+    name_guest = input('Имя гостя: ')
+
+    if ask == 'пришел':
+        came(name_guest)
+    elif ask == 'ушел':
+        gone(name_guest)
+    print('Гость пришел или ушел? - ', end='')
+    ask = input()
+
+    if ask == end:
+        print('\nВечеринка закончилась, все легли спать.')
+        break
