7e39089d7fd6c04bcf08705c4240ac70f7b09d8f
Sergey Sinkov <subomolemu@gmail.com>
2021-11-23 11:55:42 +0300

    Модуль 16. Решение задачи 2. Использовал метод sorted

--- Module16/02_line/main.py
+++ Module16/02_line/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+first = list(range(160, 176, 2))
+second = list(range(162, 180, 3))
+for num in second:
+    first.append(num)
+
+first = sorted(first)
+print(first)
+
+# Изучаю книгу Эрика Мэтиза, решил попробовать метод сортировки оттуда. Если так делать на данном
+# этапе нельзя, то переделаю
