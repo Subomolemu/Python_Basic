bab13c8e285fbcfe3f4c8761b2295d32e987ed9e
Sergey Sinkov <subomolemu@gmail.com>
2021-11-25 23:35:31 +0300

    Модуль 16. Решение задачи 8.

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+people = int(input('Количество человек: '))
+drop_number = int(input('Какое число в считалке: '))
+print('Значит выбывает каждый 7 человек.')
+
+people_list = list(range(1, people + 1))
+i_number = people_list[0]
+transfer_list = list(range(1, people + 1))
+
+while len(people_list) > 1:
+
+    print('\nТекущий круг людей: ', transfer_list)
+    print('Начало счета с номера:', people_list[0])
+    i_number = drop_number
+
+    if i_number > len(people_list):
+        i_number %= len(people_list)
+
+    print('Выбывает человек с номером:', people_list[i_number - 1])
+    transfer_list.remove(people_list[i_number - 1])
+    people_list.remove(people_list[i_number - 1])
+    countdown = people_list[i_number - 1]
+
+    while people_list[0] != countdown:
+        num_transfer = people_list[0]
+        people_list.remove(people_list[0])
+        people_list.append(num_transfer)
+
+print('\nОстался человек под номером:', people_list[0])
