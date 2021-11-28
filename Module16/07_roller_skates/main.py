0451cdcdc6f417d6c25ffb09d925a6c2acbb4ad1
Sergey Sinkov <subomolemu@gmail.com>
2021-11-28 19:11:55 +0300

    Модуль 16. Обновленное решение задачи 7.

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,6 +1,7 @@
 total_rollers = int(input('Введите количество коньков: '))
 count_people = 0
 rollers = []
+leg_sizes = []
 
 for i_roll in range(total_rollers):
     print('Размер', i_roll + 1, 'пары: ', end='')
@@ -8,15 +9,23 @@ for i_roll in range(total_rollers):
     rollers.append(size)
 print()
 
+rollers.sort()
+print(rollers)
+
 peoples = int(input('Введите количество людей: '))
+print()
 
 for people in range(peoples):
     print('Размер ноги', people + 1, 'человека: ', end='')
     leg_size = int(input())
-    for size in rollers:
-        if size == leg_size:
-            count_people += 1
-            rollers.remove(size)
+    leg_sizes.append(leg_size)
+
+leg_sizes.sort()
+
+for i_leg_size in leg_sizes:
+    for roll in rollers:
+        if i_leg_size <= roll:
+            rollers.remove(roll)
             break
 
-print('\nНаибольшее количество людей, который могут взять ролики:', count_people)
+print('\nНаибольшее количество людей, который могут взять ролики:', total_rollers - len(rollers))
