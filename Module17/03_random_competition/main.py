dcff900ebdfb8a39ebf48728892bc72b37123c5b
Sergey Sinkov <subomolemu@gmail.com>
2021-11-29 18:35:22 +0300

    Модуль 17. Решение задачи 3

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import random
+
+list_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+list_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
+print('Первая команда:', list_1)
+print('Вторая команда:', list_2)
+total_list = [list_1[x] if list_1[x] >= list_2[x] else list_2[x] for x in range(20)]
+print('Победители тура:', total_list)
