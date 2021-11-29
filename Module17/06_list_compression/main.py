188d3ecca7a74443816c8f318b66a1b25ef845a3
Sergey Sinkov <subomolemu@gmail.com>
2021-11-29 18:37:03 +0300

    Модуль 17. Решение задачи 6

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+import random
+
+num = int(input('Введите длину списка чисел: '))
+
+numbers = [random.randint(0, 5) for _ in range(num)]
+print(numbers)
+
+shift = [(x if x != 0 else numbers.append(numbers.pop(numbers.index(x)))) for x in
+         numbers]
+print(numbers)
+
+while numbers[-1] == 0:
+    numbers.remove(numbers[-1])
+print(numbers)
