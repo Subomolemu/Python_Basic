92502c75470f21503339783619f23ab36aaad080
Sergey Sinkov <subomolemu@gmail.com>
2021-11-29 18:43:09 +0300

    Модуль 17. Решение задачи 10

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def check(symbol, alpha, i_shift):
+    while alpha.index(symbol) + i_shift > 33:
+        index = (alpha.index(symbol) + i_shift) % len(alpha)
+        return index
+    else:
+        index = (alpha.index(symbol) + i_shift)
+        return index
+
+
+text = input('Введите текст: ')
+shift = int(input('Введите сдвиг: '))
+alphabet = 'абвгдеёжзийклмyнопрстуфхцчшщъыьэюя'
+new_text = [alphabet[check(sym, alphabet, shift)] if sym in alphabet else ' ' for sym in text]
+for i_sym in new_text:
+    print(i_sym, end='')
