b700b55146dcfcafc340608de619ec455335ba13
Sergey Sinkov <subomolemu@gmail.com>
2021-12-04 23:27:33 +0300

    Модуль 18. Решение задачи 2.

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+# дом машина лес он
+text = input('Введите текст через пробелы: ').split()
+total_len = 0
+total_word = ''
+
+for word in text:
+    len_str = len(word)
+    if len_str > total_len:
+        total_len = len_str
+        total_word = word
+
+print('\nСамое длинное слово: {},\nЕго длина: {}'.format(
+    total_word,
+    total_len
+))
