1a82798b5393cedb4ff4d9c8699dc28ad0836535
Sergey Sinkov <subomolemu@gmail.com>
2021-12-05 01:08:44 +0300

    Модуль 18. Решение задачи 6.

--- Module18/06_compression/main.py
+++ Module18/06_compression/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+last_sym = ''
+
+for i in range(len(string)):
+    if string[i] != last_sym:
+        print(string[i]+str(string.count(string[i])), end='')
+        last_sym = string[i]
