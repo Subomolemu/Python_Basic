949d5467852b1a4fab56471c4d24fbd81e833950
Sergey Sinkov <subomolemu@gmail.com>
2021-12-05 18:09:19 +0300

    Модуль 18. Решение задачи 9.

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+text = input('Сообщение: ')
+revers_text = []
+i_revers = []
+
+for sym in text:
+    if sym.isalpha():
+        i_revers.append(sym)
+    else:
+        i_revers.reverse()
+        revers_text.append(i_revers)
+        revers_text.append(sym)
+        i_revers = []
+
+open_revers_text = [i for sym in revers_text for i in sym]
+new_text = ''.join(open_revers_text)
+print('Новое сообщение:', new_text)
