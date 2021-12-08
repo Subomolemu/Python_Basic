a8dfde81c5db1eac0207e68d7ab0cb2d97872a2c
Sergey Sinkov <subomolemu@gmail.com>
2021-12-08 19:36:50 +0300

    Модуль 18.Правка к решению задачи 10.

--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,53 +1,54 @@
 def check(symbol, alpha, i_shift):
     while alpha.index(symbol) + i_shift > 25:
-        index = (alpha.index(symbol) + i_shift) % len(alpha)
-        return index
+        index_sym = (alpha.index(symbol) + i_shift) % len(alpha)
+        return index_sym
     else:
-        index = (alpha.index(symbol) + i_shift)
-        return index
+        index_sym = (alpha.index(symbol) + i_shift)
+        return index_sym
 
 
-flag = False
-finish_text = []
-a = 3
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju'.lower()
+text = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ' \
+       'ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt ' \
+       'uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu ' \
+       't/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdj' \
+       'umzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip ' \
+       'fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf w' \
+       'jpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu ' \
+       'cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg ' \
+       'fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ' \
+       'ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip'.lower()
+
 shift = 25
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 new_text = [alphabet[check(sym, alphabet, shift)] if sym in alphabet else sym for sym in text]
-print(new_text)
-text_str = ''.join(new_text)
-print(text_str)
-text_str = text_str.split()
+new_text = ''.join(new_text).split()
+text = []
 
-text_str = [text_str]
 
-for i_text in text_str:
+x = 3  # сдвиг символов в строке
+a = [sym for sym in new_text[0]]  # рабочая строка для реализации сдвига строки в списке
 
-    for lang in range(len(i_text)):
-        i = i_text[lang]
+for i in range(1, len(new_text)):
+    index = new_text[i]
 
-        for sym in range(a):
-            i = [symbol for symbol in i]
-            popped_sym = i.pop()
-            i.insert(0, popped_sym)
-            i = ''.join(i)
+    for _ in range(x):
+        popped_sym = a.pop()
+        a.insert(0, popped_sym)
 
-        index_sym = i_text.index(i_text[lang])
-        i_text.remove(i_text[lang])
-        i_text.insert(index_sym, i)
+    a = ''.join(a)
+    new_text.pop(i - 1)
+    new_text.insert(i - 1, a)
 
-    for ind in i_text:
-        for i in ind:
-            if i == '/':
-                flag = True
-                i_ind = [sym for sym in ind if sym != '/']
-                i_ind = ''.join(i_ind)
-                finish_text.append(i_ind)
-        if flag:
-            break
-        finish_text.append(ind)
-    a += 1
+    a = [sym for sym in new_text[i]]
+    if '/' in a:
+        x += 1
 
-print(finish_text)
-
-print(text_str)
+print('Дзен питона:\n-', end=' ')
+for i in new_text:
+    if '/' not in i:
+        print(i, end=' ')
+    else:
+        no_slash_i = [sym for sym in i if sym != '/']
+        no_slash_i = ''.join(no_slash_i)
+        print(no_slash_i)
+        print('-', end=' ')
