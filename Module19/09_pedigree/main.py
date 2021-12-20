ab3879ad7411615736a5a8388fcb9221716b5929
Sergey Sinkov <subomolemu@gmail.com>
2021-12-20 18:22:52 +0300

    Модуль 19. Решение задачи 9.

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+total_people = int(input('Введите количество человек: '))
+tree = {}
+
+for i in range(1, total_people):
+    child, parent = input(f'{i} пара: ').split()
+    if parent not in tree:
+        tree[parent] = 0
+    if child not in tree:
+        tree[child] = tree[parent] + 1
+
+print('\nВысота каждого члена семьи: ')
+for person, lvl in sorted(tree.items()):
+    print(f'{person} {lvl}')
