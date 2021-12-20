479b1861d492703f6d359dfcd14cb9f4df5c4068
Sergey Sinkov <subomolemu@gmail.com>
2021-12-20 18:42:43 +0300

    Модуль 19. Решение задачи 10.

--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+count = 0
+set_string = set(string)
+
+for sym in set_string:
+    if string.count(sym) % 2 != 0:
+        count += 1
+
+if count >= 2:
+    print('Нельзя сделать палиндромом')
+else:
+    print('Можно сделать палиндромом')
+
+
+
