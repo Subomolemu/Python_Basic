56abc995b4ac2a78602e0a38d0d9a65ac652292c
Sergey Sinkov <subomolemu@gmail.com>
2021-12-22 23:33:38 +0300

    Модуль 20. Решение задачи 5

--- Module20/05_one_family/main.py
+++ Module20/05_one_family/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+dict_family = {
+    ('Сидоров', 'Никита'): 25,
+    ('Сидорова', 'Анна'): 28,
+    ('Петров', 'Владислав'): 32,
+    ('Петрова', 'Алена'): 30,
+    ('Петров', 'Андрей'): 41,
+    ('Иванов', 'Олег'): 39,
+    ('Иванова', 'Анастасия'): 34,
+    }
+
+chose_surname = input('Введите фамилию: ').lower().title()
+
+for f_n, age in dict_family.items():
+    surname = f_n[0]
+    if surname[:len(chose_surname)] == chose_surname:
+        print(f'{f_n[0]} {f_n[1]}, {age} лет.')
+    elif surname[0] == chose_surname[:len(surname[0])]:
+        print(f'{f_n[0]} {f_n[1]}, {age} лет.')
