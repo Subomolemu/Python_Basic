52dcc1fd3d66cc82d7e0f5b5c7487eb78908e5c5
Sergey Sinkov <subomolemu@gmail.com>
2021-12-26 22:34:26 +0300

    Модуль 20. Правка к решению задачи 10

--- Module20/10_my_zip/main.py
+++ Module20/10_my_zip/main.py
@@ -1,16 +1,10 @@
 def f_zip(a, b):
-    tpl = []
-    if len(a) < len(b):
-        for i in range(len(a)):
-            tpl.append((a[i], b[i]))
-        return tuple(tpl)
-    else:
-        for i in range(len(b)):
-            tpl.append((a[i], b[i]))
-        return tpl
+    min_len = min(len(a), len(b))
+    tpl = ((a[i], b[i]) for i in range(min_len))
+    return tpl
 
 
-string = 'abcd'
+string = 'abc'
 num_tpl = (10, 20, 30, 40)
 
 lst = (i for i in zip(string, num_tpl))
@@ -19,4 +13,6 @@ print(lst)
 for i in lst:
     print(i)
 
-print(f_zip(string, num_tpl))
+print(f'\n{f_zip(string, num_tpl)}')
+for i in f_zip(string, num_tpl):
+    print(i)
