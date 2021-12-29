93c03a0a4826b94110d8b2da8c3914a8d9c83650
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 15:28:04 +0300

    Модуль 21. Решение задачи 1.

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def outputs_numbers(number, printed_number):
+    if printed_number == number:
+        return number
+    print(printed_number)
+    return outputs_numbers(number, printed_number + 1)
+
+
+num = int(input('Введите число: '))
+printed_num = 1
+print(outputs_numbers(num, printed_num))
