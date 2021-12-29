dfb223b24b02afb5279568928b8f1ac7aaf44729
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 16:03:35 +0300

    Модуль 21. Решение задачи 2.

--- Module21/02_my_zip_2/main.py
+++ Module21/02_my_zip_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def analog_zip(object_1, object_2):
+    desired_length = min(len(date_1), len(date_2))
+    tpl = [(object_1[sym], object_2[sym])
+           for i, sym in enumerate(range(desired_length))]
+    return tpl
+
+
+date_1 = '312'
+date_2 = ('a', 'b', 12, 22)
+
+print(analog_zip(date_1, date_2))
