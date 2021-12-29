68ff5c07f5181f3dec4c9a43cf853ede7da8538a
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 19:43:42 +0300

    Модуль 21. Решение задачи 6.

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def summa(*args, my_list=[]):
+	for i in args:
+		if isinstance(i, list):
+			for index in i:
+				summa(index)
+		else:
+			my_list.append(i)
+
+	return sum(my_list)
+
+
+print(summa([[1, 2, [3]], [1], 3]))
