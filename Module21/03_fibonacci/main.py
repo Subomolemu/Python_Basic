81c59a19ab393816ccf7468fde78967436e60b86
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 16:46:49 +0300

    Модуль 21. Решение задачи 3.

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def fib_num(num, it_num):
+    if len(list_num) == num:
+        return list_num[-1]
+    list_num.append(it_num)
+    return fib_num(num, it_num=list_num[-1] + list_num[-2])
+
+
+list_num = [1]
+iter_num = 1
+number = int(input('Введите позицию числа в ряде Фибоначчи: '))
+print(f'\nЧисло: {fib_num(number, iter_num)}')
