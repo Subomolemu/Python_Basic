8c1e729afef565d80600ee9c83411d3b9108125a
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 22:23:24 +0300

    Модуль 21. Решение задачи 9.

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def move_disk(disk, start=1, stop=3):
+    if disk == 1:
+        print(f'Переложить диск {disk} со стержня номер {start} '
+              f'на стержень номер {stop}')
+    else:
+        move_disk(disk - 1, start, 6 - start - stop)
+        print(f'Переложить диск {disk} со стержня номер {start} '
+              f'на стержень номер {stop}')
+        move_disk(disk - 1, 6 - start - stop, stop)
+
+
+total_disks = int(input('Введите количество дисков:'))
+move_disk(total_disks)
