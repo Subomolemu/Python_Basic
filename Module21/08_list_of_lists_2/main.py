c98e076d9d06bdfb7d470b6381d618395fa35287
Sergey Sinkov <subomolemu@gmail.com>
2021-12-29 19:54:10 +0300

    Модуль 21. Решение задачи 8.

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,16 @@
+def return_list(lst, out_list=[]):
+    if isinstance(lst, list):
+        for i in lst:
+            if isinstance(i, list):
+                for index in i:
+                    return_list(index)
+
+            else:
+                out_list.append(i)
+    return out_list
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print(return_list(nice_list))
