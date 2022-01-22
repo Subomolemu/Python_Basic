02df01e1aeb63d4e3c76a37f0275aa0ec09f3719
Sergey Sinkov <subomolemu@gmail.com>
2022-01-22 13:54:18 +0300

    Модуль 24. Правка к решению задачи 7

--- Module24/07_cohabitation/human.py
+++ Module24/07_cohabitation/human.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Human:
     satiety = 50
     
@@ -39,3 +42,26 @@ class Human:
                   f'Вы умерли.')
             return False
         
+    def act(self):
+        check = random.randint(1, 6)
+        if self.satiety < 20:
+            self.eat()
+        elif self.house.food < 10:
+            self.buy_food()
+        elif self.house.money < 50:
+            self.work()
+        elif check == 1:
+            self.work()
+        elif check == 2:
+            self.eat()
+        else:
+            self.play()
+
+    def input_info(self):
+        print(f'\nИнформация о жителях дома:\n'
+              f'\t-Меня зовут {self.name}\n'
+              f'\t-Моя сытость находится на уровне {self.satiety} единиц\n'
+              f'\t-В тумбочке находится {self.house.money} единиц денег\n'
+              f'\t-В холодильнике находится {self.house.food} единиц еды\n'
+              f'\t-Меня можно поздравить, ведь я выжил(а) при таких условиях '
+              f'проживания!')
--- Module24/07_cohabitation/main.py
+++ Module24/07_cohabitation/main.py
@@ -1,47 +1,19 @@
-import random
 import house
 import human
-
-
-def human_logic(hum):
-    check = random.randint(1, 6)
-    if hum.satiety < 20:
-        hum.eat()
-    elif hum.house.food < 10:
-        hum.buy_food()
-    elif hum.house.money < 50:
-        hum.work()
-    elif check == 1:
-        hum.work()
-    elif check == 2:
-        hum.eat()
-    else:
-        hum.play()
-
-
-def input_info(hum):
-    print(f'\nИнформация о жителях дома:\n'
-          f'\t-Меня зовут {hum.name}\n'
-          f'\t-Моя сытость находится на уровне {hum.satiety} единиц\n'
-          f'\t-В тумбочке находится {hum.house.money} единиц денег\n'
-          f'\t-В холодильнике находится {hum.house.food} единиц еды\n'
-          f'\t-Меня можно поздравить, ведь я выжил(а) при таких условиях '
-          f'проживания!')
 
 
 my_house = house.House
 human_1 = human.Human('Sergey', my_house)
 human_2 = human.Human('Elena', my_house)
-day = 0
 
-while day != 365:
-    day += 1
-    print(f'\nНачался {day} день.')
-    human_logic(human_1)
-    human_logic(human_2)
-    if human_1.check_life() or human_2.check_life():
-        break
-        
-if day == 365:
-    input_info(human_1)
-    input_info(human_2)
+humans = [human_1, human_2]
+
+for day in range(365):
+    for human in humans:
+        print(f'\nНачался {day + 1} день.')
+        human.act()
+        if human_1.check_life() or human_2.check_life():
+            break
+            
+for human in humans:
+    human.input_info()
