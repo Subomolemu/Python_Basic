3bc26ba9a174e244d7b4572e9a8b0dc87bb110d9
Sergey Sinkov <subomolemu@gmail.com>
2022-01-27 19:17:44 +0300

    Модуль 25. Решение задачи 3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+class MyDict(dict):
+    def __init__(self):
+        super().__init__()
+
+    def get(self, i):
+        if i in MyDict.keys(self):
+            for key, value in MyDict.items(self):
+                if key == i:
+                    return value
+        else:
+            return 0
+
+            
+my_dict = MyDict()
+my_dict[2] = 1
+my_dict[3] = 3
+print(my_dict)
+print(my_dict.get(1))
+
+
+
