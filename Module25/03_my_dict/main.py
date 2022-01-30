2cc2bbb015b73fddc4519cfcefcb7e252c328f92
Sergey Sinkov <subomolemu@gmail.com>
2022-01-30 18:25:00 +0300

    Модуль 25. Правка к решению задачи 3

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,21 +1,10 @@
 class MyDict(dict):
-    def __init__(self):
-        super().__init__()
-
-    def get(self, i):
-        if i in MyDict.keys(self):
-            for key, value in MyDict.items(self):
-                if key == i:
-                    return value
-        else:
-            return 0
-
-            
+    def get(self, key, default=0):
+        return super().get(key, default)
+        
+        
 my_dict = MyDict()
 my_dict[2] = 1
 my_dict[3] = 3
 print(my_dict)
 print(my_dict.get(1))
-
-
-
