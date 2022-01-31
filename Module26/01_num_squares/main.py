fe6342113bc2175d9cd850b7c126d67dab3663d4
Sergey Sinkov <subomolemu@gmail.com>
2022-01-31 18:07:38 +0300

    Модуль 26. Решение задачи 1

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+class NumSquare:
+    def __init__(self, num: int):
+        self.start_num = 0
+        self.num = num
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        if self.start_num != self.num:
+            self.start_num += 1
+            return self.start_num ** 2
+        raise StopIteration
+    
+    
+def num_square(num):
+    start_num = 0
+    while True:
+        if start_num != num:
+            start_num += 1
+            yield start_num ** 2
+        else:
+            break
+    
+    
+my_number = int(input('Введите конечное число: '))
+for number in NumSquare(my_number):
+    print(number, end=' ')
+print('\n')
+for number in num_square(my_number):
+    print(number, end=' ')
+print('\n')
+number = [num**2 for num in range(1, my_number + 1)]
+for num in number:
+    print(num, end=' ')
