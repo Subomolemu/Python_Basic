fa2714b6c31aefab782a4170b12b8eb819b0838c
Sergey Sinkov <subomolemu@gmail.com>
2022-01-31 20:38:25 +0300

    Модуль 26. Решение задачи 4

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+class QHofstadter:
+    def __init__(self, s):
+        self.s = s
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.s == [1, 2]:
+            raise StopIteration
+        else:
+            q = self.s[-self.s[-1]] + self.s[-self.s[-2]]
+            self.s.append(q)
+            return q
+
+
+q = QHofstadter([1, 1])
+for i in range(10):
+    print(next(q))
