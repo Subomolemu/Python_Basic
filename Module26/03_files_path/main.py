9224f5e6d28ea506ce269fe646b73a3098e02123
Sergey Sinkov <subomolemu@gmail.com>
2022-02-02 22:38:46 +0300

    Модуль 26. Правка к решению задачи 3

--- Module26/03_files_path/main.py
+++ Module26/03_files_path/main.py
@@ -8,19 +8,18 @@ def gen_files_path(path: str, find_path: str):
             if i_elem == find_path:
                 for i in os.listdir(next_path):
                     yield f'{os.path.join(next_path, i)}'
-            if os.path.isdir(next_path):
-                for i in os.listdir(next_path):
-                    new_path = os.path.join(next_path, i)
-                    if i == find_path:
-                        for i_path in os.listdir(new_path):
-                            yield f'{os.path.join(new_path, i_path)}'
-                    gen_files_path(new_path, find_path)
+            else:
+                if os.path.isdir(next_path):
+                    for new_path in gen_files_path(next_path, find_path):
+                        yield f'{os.path.join(next_path, new_path)}'
         except PermissionError:
             continue
 
 
 start_path = os.path.abspath(os.path.join('/'))
-my_path = 'Practice'
+my_path = 'Data'
 found_path = gen_files_path(path=start_path, find_path=my_path)
-for found_path in found_path:
-    print(found_path)
+print(found_path)
+for my_path in found_path:
+    print(my_path)
+    
