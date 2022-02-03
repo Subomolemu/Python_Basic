bdd05230ad28af2181c1bbdf85061fd55a5696fa
Sergey Sinkov <subomolemu@gmail.com>
2022-02-03 19:39:06 +0300

    Модуль 27. Решении задачи 2

--- Module27/02_slowdown/main.py
+++ Module27/02_slowdown/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+from typing import Callable
+import time
+
+
+def slowdown(func: Callable) -> Callable:
+    time.sleep(2)
+    return func
+
+
+@slowdown
+def say_hello(name: str) -> str:
+    return f'Hallo, {name}'
+
+
+my_name = 'Sergey'
+func_hello = say_hello(my_name)
+print(func_hello)
