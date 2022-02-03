794f76bab3bebdea8a0ea6f8de609f8b7365c593
Sergey Sinkov <subomolemu@gmail.com>
2022-02-03 20:38:04 +0300

    Модуль 27. Решении задачи 3

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,1 +1,36 @@
-# TODO здесь писать код
+from typing import Callable, Any
+import functools
+import datetime
+
+
+def logging(func: Callable) -> Callable:
+    """ Декоратор выполняет логирование функции, если возникла ошибка, запишет
+    ее в файл 'function_errors.log' """
+    print(f'Документация: {func.__doc__}')
+    print(f'Название функции: {func.__name__}')
+
+    @functools.wraps(func)
+    def wrapped_func(*args, **kwargs) -> Any:
+        res = func(*args, **kwargs)
+        return res
+    return wrapped_func
+
+    
+@logging
+def square_num(num: int) -> int:
+    """Функция для возведения числа в квадрат"""
+    
+    return num ** 2
+    
+
+try:
+    number = 's'
+    square_number = square_num(number)
+    print(square_number)
+
+except TypeError:
+    with open('function_errors.log', 'a+') as error:
+        error.write(f'{square_num.__name__}('
+                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")}'
+                    f') - "TypeError"\n')
+        
