a1ab223142040146959abeed075a72052f8013d7
Sergey Sinkov <subomolemu@gmail.com>
2022-02-03 19:35:43 +0300

    Модуль 27. Решении задачи 1

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,34 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def rise_question(func: Callable) -> Callable:
+    """ Декоратор для вывода вопроса "как дела?" """
+    
+    def wrapped_func(*args, **kwargs) -> Any:
+        answer = input('Как дела?\n- ')
+        print('А у меня не очень! Ладно, держи свою функцию.')
+        res = func(*args, **kwargs)
+        return res
+    return wrapped_func
+    
+    
+@rise_question
+def sum_square(number: int) -> int:
+    summa = 0
+    for i in range(1, number + 1):
+        summa += i ** 2
+    return summa
+
+
+@rise_question
+def say_hello(name: str) -> str:
+    return f'Hallo, {name}'
+
+
+my_name = 'Sergey'
+num = 10
+my_func = sum_square(num)
+print(my_func)
+print()
+func_hello = say_hello(my_name)
+print(func_hello)
