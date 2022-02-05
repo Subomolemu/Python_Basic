75082cedf562887277f8a8e7db3bf45f457ad2c3
Sergey Sinkov <subomolemu@gmail.com>
2022-02-05 21:18:37 +0300

    Модуль 27. Правка к решению задачи 3

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -3,34 +3,35 @@ import functools
 import datetime
 
 
-def logging(func: Callable) -> Callable:
+def logging(func: Callable) -> [Callable, str]:
     """ Декоратор выполняет логирование функции, если возникла ошибка, запишет
     ее в файл 'function_errors.log' """
-    print(f'Документация: {func.__doc__}')
-    print(f'Название функции: {func.__name__}')
-
+    
     @functools.wraps(func)
     def wrapped_func(*args, **kwargs) -> Any:
-        res = func(*args, **kwargs)
-        return res
+        try:
+            print(f'Документация: {func.__doc__}')
+            print(f'Название функции: {func.__name__}')
+            
+            res = func(*args, **kwargs)
+            return res
+        except TypeError:
+            date_now = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
+            with open('function_errors.log', 'a+') as error:
+                error.write(f'{square_num.__name__}('
+                            f'{date_now}'
+                            f') - "TypeError"\n')
+    
     return wrapped_func
 
-    
+
 @logging
 def square_num(num: int) -> int:
     """Функция для возведения числа в квадрат"""
     
     return num ** 2
-    
 
-try:
-    number = 's'
-    square_number = square_num(number)
-    print(square_number)
 
-except TypeError:
-    with open('function_errors.log', 'a+') as error:
-        error.write(f'{square_num.__name__}('
-                    f'{datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")}'
-                    f') - "TypeError"\n')
-        
+number = 's'
+square_number = square_num(number)
+print(square_number)
