35d89c4b008a1898ca1825a5a0f8449c9fa65074
Sergey Sinkov <subomolemu@gmail.com>
2022-02-06 21:25:20 +0300

    Модуль 29. Решение задачи 2

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,33 @@
-# TODO здесь писать код
+import functools
+from typing import Callable, Any
+
+
+def callback(key: Any) -> Callable:
+    def rise_func(func: Any) -> Callable:
+        
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs) -> str:
+            res = func(*args, **kwargs)
+            app[key] = res
+            return res
+        return wrapped
+    return rise_func
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {}
+example()
+route = app.get('//')
+if route:
+    response = route
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
+
+# Пример функции, которая возвращает ответ сервера
+# Ответ: OK
