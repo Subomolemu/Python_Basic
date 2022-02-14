54d33e9d560e8ab6cf4ae91a23b7ef491e27733d
Sergey Sinkov <subomolemu@gmail.com>
2022-02-14 21:49:38 +0300

    Модуль 30, решение задачи 4

--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+from typing import List
+
+
+def import_list() -> List:
+    lst = list()
+    for i in range(1001):
+        lst.append(i)
+    return lst
+
+
+print(list(map(lambda x: x, [x for x in range(1001)])))
+
+print(import_list())
+
+print(list(x for x in range(1001)))  # самый простой и читаемый, как по мне
