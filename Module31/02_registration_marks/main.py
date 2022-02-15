389a202fe315048b65876667408dfd6e385392eb
Sergey Sinkov <subomolemu@gmail.com>
2022-02-15 19:41:34 +0300

    Модуль 31. Решение задачи 2

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+import re
+
+
+auto_num = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+auto = re.findall(r'\b\w\d{3}\w{2}\d{2,3}', auto_num)
+print(f'Список номеров частных автомобилей: {auto}')
+taxi = re.findall(r'\b\w{2}\d{5,6}', auto_num)
+print(f'Список номеров такси: {taxi}')
