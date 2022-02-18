378ac48f90af95bed2bb7a1e7ecfa26c60d8c9fd
Sergey Sinkov <subomolemu@gmail.com>
2022-02-18 21:42:44 +0300

    Модуль 31. Решение задачи 6

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import requests
+import re
+
+my_request = requests.get('http://www.columbia.edu/~fdc/sample.html')
+
+date_list = re.findall(r'>\b.+\b<.h3>', my_request.text)
+
+new_list = list(map(lambda string: ''.join(re.findall(r'.', string))[1:-5], date_list))
+print(new_list)
